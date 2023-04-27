53d5da14e728a8980174569e66b5ad7cc76e177d
danilojpfreitas <danilojpfreitas@gmail.com>
2023-04-27 18:23:26 +0000

    Second commit

---
+++ target/run/analytics_dbt/models/views/vw_sales.sql
@@ -0,0 +1,26 @@
+
+
+  create view "dev"."public"."vw_sales__dbt_tmp" as (
+    with source_sales as (
+    select *
+    from sales
+),
+
+renamed as (
+    select 
+        SALESID AS ID_VENDA,
+        SELLERID AS ID_LISTA,
+        BUYERID AS ID_COMPRADOR,
+        EVENTID AS ID_EVENTO,
+        DATEID AS ID_DATE,
+        QTYSOLD AS QUANTIDADE_VENDIDA,
+        PRICEPAID AS VALOR_PAGO,
+        COMMISSION AS COMISSÃO,
+        TO_CHAR(SALETIME, 'dd/mm/yyyy HH24:MI:SS') AS DATA_HORA_VENDA,
+        TO_CHAR(SALETIME, 'HH24:MI:SS') AS HORA_VENDA,
+        cast(SALETIME as date) AS DATA_VENDA
+    from source_sales
+)
+
+select * from renamed
+  ) ;
